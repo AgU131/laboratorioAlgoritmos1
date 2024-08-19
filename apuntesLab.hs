bdd9480555ff22c9436a9032c13b00ef3ab1cc66
Agustin <82238717+AgU131@users.noreply.github.com>
2024-08-19 12:55:53 -0300

    Add files via upload
    actualizado final

--- apuntesLab.hs
+++ apuntesLab.hs
@@ -11,7 +11,7 @@
 -- comandos basicos para navegar en linux: abrir terminalctrl alt t
 --	ls listar.    cd   (change directory) + tab. ll (no se que hace).   cd (solo vuelve al home) y con ../ vuelve al respositorio anterior.    rm "archivo.hs"  (ELIMINA ARCHIVO).    mv "nombre.hs" "nuevonombre.hs" (cambia el nombre del archivo).     tree "repositorio/lugar en donde estas" (hace un arbol de lo que contiene la carpeta).    mkdir "nombre" (crea carpeta).    touch "nombre.hs" (crea doc de texto).   
 --  ghci "nombre del archivo.hs" (compila el archivo).   :l "nombre.hs" (:l==:load, carga el archivo dentro de ghci).    :r (:r==:reload, recarga el archivo previamente cargado).   :q (salir de ghci).    ctrl + d (salir de ghci o de algo de la terminal).   ctrl + l (limpia la pantalla de la terminal)
--- maxBound o minBound (hace max o min hasta infinito).    :t "funcion" (me da el tipo de lafuncion).      :i "Tipo" (d informacion del Tipo)
+-- maxBound o minBound (hace max o min hasta infinito).    :t "funcion" (me da el tipo de lafuncion).      :i "Tipo" (da informacion del Tipo).     
 
 {-
 ghc es el compiolador, siendo que ghci es el interprete
