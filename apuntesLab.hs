7bd3f93e8e755419cfa9a980dc8dedee0672fc1f
Agustin <82238717+AgU131@users.noreply.github.com>
2024-08-19 11:52:51 -0300

    Add files via upload

---
+++ apuntesLab.hs
@@ -0,0 +1,13 @@
+    --Prueba/ practica de haskell again babyyY!!!
+     
+ esCero :: Int -> Bool
+ esCero x = x==0
+   
+ esPositivo :: Int -> Bool
+ esPositivo x  x > 0
+ 
+ 
+
+-- comandos basicos para navegar en linux: abrir terminalctrl alt t
+--	ls listar.    cd   (change directory) + tab. ll (no se que hace).   cd (solo vuelve al home) y con ../ vuelve al respositorio anterior.    rm "archivo.hs"  (ELIMINA ARCHIVO).    mv "nombre.hs" "nuevonombre.hs" (cambia el nombre del archivo).     tree "repositorio/lugar en donde estas" (hace un arbol de lo que contiene la carpeta).    mkdir "nombre" (crea carpeta).    touch "nombre.hs" (crea doc de texto).   
+--  ghci "nombre del archivo.hs" (compila el archivo).   :l "nombre.hs" (:l==:load, carga el archivo dentro de ghci).    :r (:r==:reload, recarga el archivo previamente cargado).   :q (salir de ghci).    ctrl + d (salir de ghci o de algo de la terminal).   ctrl + l (limpia la pantalla de la terminal)
---
+++ lab1.hs
@@ -0,0 +1,72 @@
+--  Arrancamos con haskel!!! en el lab
+             --   19/082024
+
+-- ejercicio lab 1
+esCero :: Int -> Bool
+esCero x = x==0
+
+
+esPositivo :: Int -> Bool
+esPositivo x = x>0
+
+
+esVocal :: Char -> Bool
+esVocal a = a=='a' || a=='e' || a=='i' || a=='o' || a=='u'
+
+-- AltGr + 1 = | 
+-- || = or
+
+valorAbsoluto  :: Int -> Int
+valorAbsoluto x 
+   | (x >=0) = x 
+   | otherwise = -x
+{- OTRA FORMA
+
+valorAbsoluto x =
+                 if (x>=0) then x 
+                  else (-x)       -}
+
+
+-- ejercicio lab 2
+
+paraTodo :: [Bool] -> Bool
+paraTodo [] = True
+paraTodo (x:xs) = x==True && paraTodo xs
+
+sumatoria :: [Int] -> Int
+sumatoria [] = 0
+sumatoria (x:xs) = x + sumatoria xs
+
+productoria :: [Int] -> Int
+productoria [] = 1
+productoria (x:xs) = x * productoria xs 
+
+factorial :: Int -> Int
+factorial 0 = 1 
+factorial x = x * factorial (x-1)
+
+
+-- ejercicio lab 3 (usar ejercicios 4a), 4b) y 4d))
+
+--4 {paratodo i : 0<= i < #xs : xs.i>0}
+cuatroA :: [Int] -> Bool
+cuatroA [] = True
+cuatroA (x:xs)
+   | 0<=x && x < length (x:xs) = x > 0 && cuatroA xs
+   | otherwise = False
+
+
+--4b) {existe i : 0<= i < #xs : xs.i=x}
+cuatroB :: [Int] -> Bool
+cuatroB [] = True
+cuatroB (x:xs)
+   |  0<=x && x < length (x:xs) = x==fst xs && cuatroB xs
+   | otherwise = True
+
+
+--4d) {paratodo i : 0<= i < #xs-1 : xs.i= xs.(i+1)}
+
+
+
+{-
+-}
