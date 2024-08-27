94b06c02655487d930eff3732d6ad213e153c9b9
Agustin <82238717+AgU131@users.noreply.github.com>
2024-08-26 21:13:20 -0300

    Add files via upload

--- apuntesLab.hs
+++ apuntesLab.hs
@@ -1,21 +1,60 @@
-    --Prueba/ practica de haskell again babyyY!!!
-     
- esCero :: Int -> Bool
- esCero x = x==0
-   
- esPositivo :: Int -> Bool
- esPositivo x  x > 0
- 
- 
+-- Apuntes de las clases del Laboratorio
 
--- comandos basicos para navegar en linux: abrir terminalctrl alt t
---	ls listar.    cd   (change directory) + tab. ll (no se que hace).   cd (solo vuelve al home) y con ../ vuelve al respositorio anterior.    rm "archivo.hs"  (ELIMINA ARCHIVO).    mv "nombre.hs" "nuevonombre.hs" (cambia el nombre del archivo).     tree "repositorio/lugar en donde estas" (hace un arbol de lo que contiene la carpeta).    mkdir "nombre" (crea carpeta).    touch "nombre.hs" (crea doc de texto).   
---  ghci "nombre del archivo.hs" (compila el archivo).   :l "nombre.hs" (:l==:load, carga el archivo dentro de ghci).    :r (:r==:reload, recarga el archivo previamente cargado).   :q (salir de ghci).    ctrl + d (salir de ghci o de algo de la terminal).   ctrl + l (limpia la pantalla de la terminal)
--- maxBound o minBound (hace max o min hasta infinito).    :t "funcion" (me da el tipo de lafuncion).      :i "Tipo" (da informacion del Tipo).     
+--Clase 19/08/2024
 
+{-
+-comandos basicos para navegar en linux: abrir terminalctrl alt t
+  ls listar.    cd   (change directory) + tab. ll (no se que hace).   cd (solo vuelve al home) y con ../ vuelve al respositorio anterior.    rm "archivo.hs"  (ELIMINA ARCHIVO).    mv "nombre.hs" "nuevonombre.hs" (cambia el nombre del archivo).     tree "repositorio/lugar en donde estas" (hace un arbol de lo que contiene la carpeta).    mkdir "nombre" (crea carpeta).    touch "nombre.hs" (crea doc de texto).   
+-}
+{-
+-- comandos de haskell
+  ghci "nombre del archivo.hs" (compila el archivo).   :l "nombre.hs" (:l==:load, carga el archivo dentro de ghci).    :r (:r==:reload, recarga el archivo previamente cargado).   :q (salir de ghci).    ctrl + d (salir de ghci o de algo de la terminal).   ctrl + l (limpia la pantalla de la terminal)
+maxBound o minBound (hace max o min hasta infinito).    :t "funcion" (me da el tipo de lafuncion).      :i "Tipo" (da informacion del Tipo).     
+para escribir una funcion que toma 2 argumentdos en el medio usamos ``, ej: 12 `mod` 2
+
+-}
 {-
 ghc es el compiolador, siendo que ghci es el interprete
 las expresiones y funciones tienen tipos unicos
+-}
+
+
+-- Clase 26/08/2024 Teorico Laboratorio
+{- 
+Veremos: 
+-Pattern matching
+se puede poner como resultado un "error" que diga algo, ej: esVacia (x:xs) = error "Aca"
+
+
+
+-Aplicacion parcial + Alto Orden
+la aplicacion es poner la funcion y dejar un espacio para el/los argumentos, ej: esVacia []. Y en c se usas los () y pegados
+es aplicar de a un argumento a la vez a una funcion(que es una funcion valida a haskel)
+ej: esMultiplo :: Int -> Int -> Bool 
+esMultiplo m n = mod n m == 0
+
+y si ponemos :t esMultiplo 2 estamos haciendo la aplicacion parcial (el de a poco)
+
+
+esPar :: Int -> Bool
+esPar m = esMultiplo 2 m
+Tambien en Alto Orden tiene que ver con los valores del lenguaje (lo que se puede imprimir, numeros, bool, etc)
+
+esPar2 :: Int -> Bool
+esPar2 = esMultiplo 2         (lo que te voy a dar aca debe cumplir que tenga el mismo tipo)
+
+
+-polimorfismo parametrico y Ad-Hoc
+en varios casos en los que necesitamos usar exactamente la mismoa funcion pero de diferentes tipos, entra el polimorfismo
+mientras mas general es el tipo, menos funciones uno puede escribir, porque cada modificacion ya deja fuera muchos valores
+ej: headA :: [a] -> a
+headA [] =  error "bla"
+headA (x:xs) = x
+si a headA le sumamos 1 (x + 1) se nos acorta la bolsa de a para usar dejando fuera muchos valores como Bool
+e incluso si pusieramos headA :: a -> a, la unica opcion posible es del tipo headA a = a. porque no sabemos nada de esa funcion y lo que toma, etc
+de cualquier tipo sabemos muy poco
 
 
 -}
+
+
--- lab1.hs
+++ lab1.hs
@@ -1,3 +1,4 @@
+  --Prueba/ practica de haskell again babyyY!!!
 --  Arrancamos con haskel!!! en el lab
              --   19/082024
 
@@ -51,28 +52,153 @@ factorial x = x * factorial (x-1)
 --4 {paratodo i : 0<= i < #xs : xs.i>0}
 cuatroA :: [Int] -> Bool
 cuatroA [] = True
-cuatroA (x:xs)
-   | 0<=x && x < length (x:xs) = x > 0 && cuatroA xs
-   | otherwise = False
+cuatroA (x:xs) = x>0 && cuatroA xs
 
 
---4b) {existe i : 0<= i < #xs : xs.i=x}
+{- 4b) {existe i : 0<= i < #xs : xs.i=x}
 cuatroB :: [Int] -> Bool
 cuatroB [] = True
 cuatroB (x:xs)
-   |  0<=x && x < length (x:xs) = x==fst xs && cuatroB xs
-   | otherwise = True
-
+cuatroB = x==z && cuatroB xs
+-}
 
 --4d) {paratodo i : 0<= i < #xs-1 : xs.i= xs.(i+1)}
-cuadroD :: [Int] -> Bool
-cuatroD [] = True
-cuatroD (x:xs)
-   |  0<=x && x < length (x:xs) = x==fst xs && cuatroD xs        -- no listo ahun solo copy&paste del anterior
-   | otherwise = True
+cuatroD :: [Int] -> Bool
+cuatroD [] = False
+cuatroD (x:xs) = x == xs !!0 || cuatroD xs
 
 
 {-
+es imbolo que hace que saques una ubicacion de una lista es !!
+-}
+
+-- ejercicio lab 5. <paratodo i: o<=i<#xs: xs.i> (verifica que todos lo elem de una lista son true)
+todos :: [Bool] -> Bool
+todos [] = True
+todos (x:xs) = x && todos xs
+
+
+-- ejercicio lab 6 (usar ejercicios 11b), 11c) y 11d))
+
+--11b) n es el elem mas grande de xs
+f11b :: [Int] -> Int
+f11b xs = maximum xs
+
+--11c) el producto de elem pares de xs
+
+f' :: [Int] -> Int
+f' [] = 1
+f' (x:xs)
+   | even x = x * f' xs
+   | otherwise = f' xs
+
+--11d) la suma de los elem en posicion par de xs
+{- 
+f'' :: [Int] -> Int
+f'' [] = 0
+f'' (x:xs)
+   | (x:xs)!! = + f' xs
+   | otherwise = f' xs
 
 -}
--- ejercicio lab 4
+
+
+
+-- ejercicio lab 7
+
+
+paraTodo' :: [a] -> (a -> Bool) -> Bool
+paraTodo' [] t = True
+paraTodo' (x:xs) t = t x && paraTodo' xs t
+
+
+existe' :: [a] -> (a -> Bool) -> Bool
+existe' [] t = False
+existe' (x:xs) t = t x || existe' xs t
+
+
+sumatoria' :: [a] -> (a -> Int) -> Int
+sumatoria' [] t = 0
+sumatoria' (x:xs) t = t x + sumatoria' xs t
+
+
+productoria' :: [a] -> (a -> Int) -> Int
+productoria' [] t = 1
+productoria' (x:xs) t = t x * productoria' xs t 
+
+{-
+funciones de haskell
+mod x 2 determina si un numero es divisible por 2, se puede usar en casos pàra saber si es par
+-}
+-- ejercicio lab 9
+
+-- a)
+todosPares :: [Int] -> Bool   
+todosPares xs = paraTodo' xs even
+
+-- b)
+esMultiplo :: Int -> Int -> Bool 
+esMultiplo n x = mod x n == 0
+
+
+hayMultiplo :: Int -> [Int] -> Bool
+hayMultiplo n xs = existe' xs (esMultiplo n)
+
+{- Explicacion del Profe
+existe' :: [a] -> (a -> Bool) -> Bool
+xs :: [Int] 
+existe' xs :: (Int -> Bool) -> Bool
+a <-> Int
+esMultiplo n :: Int -> Bool
+existe' xs (esMultiplo n) :: Bool 
+-}
+
+-- c)
+cuadrado :: Int -> Int
+cuadrado x = x * x
+
+sumaCuadrados :: Int -> Int
+sumaCuadrados n = sumatoria' [(n-1)..0] cuadrado
+
+-- d)
+divide :: Int -> Int -> Bool 
+divide n x = mod n x == 0
+
+existeDivisor :: Int -> [Int] -> Bool  -- existe algun elemento en la lista ls que divide a n
+existeDivisor n ls = existe' ls (divide n)
+
+-- e)
+{-
+esPrimo :: Int -> Bool
+esPrimo = 
+
+
+
+-- f)
+factorial' 
+
+
+multiplicaPrimos :: [Int] -> Int
+
+
+esFib :: Int -> Bool
+
+
+todosFib :: [Int] -> Bool
+
+
+-}
+
+
+--ejercicio 10 (dada una lista de n numeros xs duplica cada valor de xs)
+
+dobleLista :: [Int] -> [Int]
+dobleLista [] = []
+dobleLista (x:xs) = x*2: dobleLista xs 
+{- recordemos que map funciona asi: 
+map (+1) [1,2,3]
+-}
+dobleLista' :: [Int] -> [Int]
+dobleLista' = map (*2)
+
+
