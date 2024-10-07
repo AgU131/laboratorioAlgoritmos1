c85ed2366e3d3d4c7176507597483c029141bd65
Agustin <82238717+AgU131@users.noreply.github.com>
2024-10-07 09:27:09 -0300

    Add files via upload

---
+++ C/apuntes_clase.c
@@ -0,0 +1,21 @@
+// Aqui tomare apuntes de C en las Clases de Algoritmos I Laboratorio 
+/* Clase 7-10-2024
+
+Como compilar en C:
+Para compilar un archivo .c escribir en la terminal: 
+        $> gcc -Wall -Wextra -std=c99 miarchivo.c -o miprograma
+Para ejecutar escribir: 
+        $> ./miprograma
+
+as
+
+
+
+
+
+
+*/
+
+
+
+
