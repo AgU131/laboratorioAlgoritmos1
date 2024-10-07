ae2df62fe6927a91bfc968c79408d6471ad5a91e
Agustin <82238717+AgU131@users.noreply.github.com>
2024-10-07 09:27:34 -0300

    Add files via upload

---
+++ C/clase1.c
@@ -0,0 +1,17 @@
+#include <stdio.h>        /* inclusiÃ³n biblioteca estandar */
+
+int main(void)            /* devuelve un int, sin argumentos */ 
+{
+    printf("Hello World!\n");
+    return 0;             /* resultado de la funciÃ³n */
+}
+
+
+
+
+
+
+
+
+
+
