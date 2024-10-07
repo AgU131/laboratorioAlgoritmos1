8a6b405ba20539de8ccf80364597a23930e30a61
Agustin <82238717+AgU131@users.noreply.github.com>
2024-10-07 10:41:36 -0300

    Update apuntes_clase.c

--- C/apuntes_clase.c
+++ C/apuntes_clase.c
@@ -12,13 +12,23 @@ Las Expresiones nunca van a modificar el estado, solo lo usaran
 
 Como Compilar:
 $> gcc -Wall -Wextra -std=c99 miarchivo.c -o miprograma
--w... parametros de warnings   std->version de C (1999)  -o -> output
+-w... -> parametros que chequean warnings   std->version de C (1999)  -o -> output
 $> ./miprograma
 lo llama al archivo para copilarlo
 
-comandos extra:
+Si no llamamos el -o, c crea un archivo por defecto a.out para darle el output
+Podria agregarle un -Werror que transformaria los warnings en errores sin compilar el docigo al encontrar uno 
+
+Comandos extra:
 $> ll -> Lista los archivos en la carpeta de C
 $> rm archivo -> elimina un archivo
+
+
+
+
+
+
+
 
 
 
