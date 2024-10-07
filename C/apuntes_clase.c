2582abed448ecdda55e14f85ac160149861c8abb
Agustin <82238717+AgU131@users.noreply.github.com>
2024-10-07 10:33:16 -0300

    Update apuntes_clase.c

--- C/apuntes_clase.c
+++ C/apuntes_clase.c
@@ -10,7 +10,15 @@ Para ejecutar escribir:
 estado ej.: {x->2}
 Las Expresiones nunca van a modificar el estado, solo lo usaran
 
+Como Compilar:
+$> gcc -Wall -Wextra -std=c99 miarchivo.c -o miprograma
+-w... parametros de warnings   std->version de C (1999)  -o -> output
+$> ./miprograma
+lo llama al archivo para copilarlo
 
+comandos extra:
+$> ll -> Lista los archivos en la carpeta de C
+$> rm archivo -> elimina un archivo
 
 
 
