cc71c699fa14ee999099d723f15c4cfbbb263e73
Agustin <82238717+AgU131@users.noreply.github.com>
2024-10-07 10:51:12 -0300

    Update apuntes_clase.c

--- C/apuntes_clase.c
+++ C/apuntes_clase.c
@@ -23,6 +23,21 @@ Comandos extra:
 $> ll -> Lista los archivos en la carpeta de C
 $> rm archivo -> elimina un archivo
 
+Tipos en C: char, int, float, double (float de doble presicion)
+Para poder usar Bools podemos usar <stdbool.h>
+
+Funcion Principales:
+siempre se debe llamar main. Y en () recibe parametros o void. 
+void es una forma explicita de decir que no devuelve nada, no es necesario ponerlo
+return 0; es una forma de decrile a C que todo esta bien, que termino
+
+int main() {                 //funcion principal
+   printf ("Hola mundo!\n")  //programa
+   return 0;                 //convencion
+}
+
+Librerias de C:
+
 
 
 
