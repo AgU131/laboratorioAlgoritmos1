0baee4511ffc63fe0281ccd310705ad5121b8972
Agustin <82238717+AgU131@users.noreply.github.com>
2024-10-07 12:01:39 -0300

    Add files via upload

---
+++ Practicos/soluciuonesPractico3enC/solucionPractico3.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(int x, int y) 
+{    
+    printf("Ingrese un valor para x\n");
+    scanf("%d",&x);
+    
+    printf("Ingrese un valor para y\n");
+    scanf("%d",&y);
+
+    printf("Su valor es x+y=%d",x,y,x+y);
+    return 0;
+}
+
+
+
