475944d90458068f300298691160a2894e458376
Agustin <82238717+AgU131@users.noreply.github.com>
2024-10-28 12:40:53 -0300

    Add files via upload

--- Practicos/soluciuonesPractico4enC/vocales.c
+++ Practicos/soluciuonesPractico4enC/vocales.c
@@ -1,10 +1,26 @@
 #include <stdio.h>
 #include <stdbool.h>
 
-bool es_vocal(char letra)
+bool es_vocal(char letra);
+//declaramos la funcion asi para que el prog sepa que existe, y asi la pueda ejecutar siendo igual que la func esta abajo
 
 
-
-char main() {
-    return 's';
+int main(void) {
+    char letrita;
+    scanf("%c", &letrita);
+            //otra forma sin usar if
+            //es_vocal(letrita)?printf("Es vocal\n"):printf("No es vocal\n");
+    if (es_vocal(letrita)) {
+        printf("Es vocal\n");
+    } else {
+        printf("No es vocal\n");
+    }
+    return 0;
 }
+
+
+
+bool es_vocal(char letra){
+    return letra == 'a' || letra == 'e'|| letra == 'i'|| letra == 'o'|| letra == 'u';
+}
+
