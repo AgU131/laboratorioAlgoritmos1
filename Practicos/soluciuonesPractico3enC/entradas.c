1f89ed55f6b0b388271384393f10b4cd31f89b13
Agustin <82238717+AgU131@users.noreply.github.com>
2024-10-31 17:04:58 -0300

    Add files via upload

--- Practicos/soluciuonesPractico3enC/entradas.c
+++ Practicos/soluciuonesPractico3enC/entradas.c
@@ -4,23 +4,25 @@
 //Ejercicio 10
 //1)
 //no estoy seguro si lo hice y entendi bien el ejercicio, pero ahi esta
+// funcion que pide al usuario un entero y lo devuelve
 int pedir_entero(char name) {
-    //pide al usuario un entero y lo devuelve
         int x;
-        printf("Ingrese un valor:\n");
+        printf("Ingrese un valor numerico:\n");
+        printf("Ese valor ingresado sera guardado en la variable '%c' \n", name);
         scanf("%d", &x);
-        printf("Ese valor ingresado sera guardado en la variable n \n");
     return (x);
 }
 
-void imprimir_entero(int x) {
+// funcion que imprime el entero
+void imprimir_entero(int x, char name) {
     //toma un entero y lo imprime
-    printf("%d \n", x);
+    printf("La variable '%c' contiene a: %d \n", name, x);
 }
 
 int main(void) {
+    char name = 'n';
     int n = pedir_entero('n');
-    imprimir_entero(n);
+    imprimir_entero(n, name);
     return 0;
 }
 
