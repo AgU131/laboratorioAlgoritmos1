e38081c9fb4a5ce1fa86818650c479fa23b890ee
Agustin <82238717+AgU131@users.noreply.github.com>
2024-10-31 18:01:20 -0300

    Add files via upload

---
+++ Practicos/soluciuonesPractico3enC/saludos.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+#include<stdbool.h>
+#include<assert.h>
+//Ejercicio 10
+//3)
+
+
+void imprimir_hola(void) {
+    printf("hola");
+    printf("hola\n");
+}
+
+void imprimir_chau(void) {
+    printf("chau");
+    printf("chau\n");
+}
+
+int main(void) {
+    imprimir_hola();
+    imprimir_chau();
+
+    return 0;
+}
+
+
