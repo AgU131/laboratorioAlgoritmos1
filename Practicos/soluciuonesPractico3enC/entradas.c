fc72cd24e1699c34ea4080da7c5545fb06bbc4aa
Agustin <82238717+AgU131@users.noreply.github.com>
2024-10-31 16:43:41 -0300

    Add files via upload

--- Practicos/soluciuonesPractico3enC/entradas.c
+++ Practicos/soluciuonesPractico3enC/entradas.c
@@ -1,20 +1,26 @@
 #include<stdio.h>
+#include<stdbool.h>
+#include<assert.h>
 //Ejercicio 10
-
+//1)
+//no estoy seguro si lo hice y entendi bien el ejercicio, pero ahi esta
 int pedir_entero(char name) {
     //pide al usuario un entero y lo devuelve
         int x;
-        printf("Necesitamos que ingrese un valor que sera guardado en su variable n\n");
-        printf("Ingrese un valor de tipo char ('algo'):\n");
+        printf("Ingrese un valor:\n");
         scanf("%d", &x);
+        printf("Ese valor ingresado sera guardado en la variable n \n");
     return (x);
 }
 
 void imprimir_entero(int x) {
     //toma un entero y lo imprime
-    printf("%d", x);
+    printf("%d \n", x);
 }
 
-
-
+int main(void) {
+    int n = pedir_entero('n');
+    imprimir_entero(n);
+    return 0;
+}
 
