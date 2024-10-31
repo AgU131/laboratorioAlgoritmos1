93cbda9cb9836d66ca2f43d7eff05bc10e2caba7
Agustin <82238717+AgU131@users.noreply.github.com>
2024-10-31 16:24:03 -0300

    Add files via upload

--- Practicos/soluciuonesPractico3enC/solucion1.c
+++ Practicos/soluciuonesPractico3enC/solucion1.c
@@ -315,7 +315,7 @@ int main81() {
 
 //b)
 
-int main() {
+int main82() {
     int x, i=2;
     bool res = true;
     printf("Ingrese un valor para x\n");
@@ -334,7 +334,86 @@ int main() {
 
 
 //Ejercicio 9
+//a)
+// (i -> -3, s -> 5, A -> [2,10,10,-1])
+int main91() {
+    int i, s, a[4];
 
+    //Pedir los valores de las variables
+    printf("Ingrese un valor para i\n");
+    scanf("%d", &i);
+    printf("Ingrese un valor para s\n");
+    scanf("%d", &s);
+    printf("Ahora deberá ingresar valores para el Arreglo\n");
+    printf("Ingrese un valor para a[0]\n");
+    scanf("%d", &a[0]);
+    printf("Ingrese un valor para a[1]\n");
+    scanf("%d", &a[1]);
+    printf("Ingrese un valor para a[2]\n");
+    scanf("%d", &a[2]);
+    printf("Ingrese un valor para a[3]\n");
+    scanf("%d", &a[3]);
+
+    i = 0; 
+    s = 0; 
+
+    while (i<4) {
+        printf("Estado numero %d: \na[%d] = %d\n", i+1, i, a[i]);
+        s = s + a[i];
+        i = i + 1;
+        printf("s = %d\n",s);
+        printf("i = %d\n",i);
+    }
+    printf("Estado final: \ni = %d \ns = %d", i, s);
+        
+    return 0;
+}
+
+
+//b)
+// (i -> 3, c -> 12, A -> [12,-9,10,-1])
+int main() {
+    int i, c; 
+    int a[4];
+
+    //Pedir los valores de las variables
+    printf("Ingrese un valor para i\n");
+    scanf("%d", &i);
+    printf("Ingrese un valor para c\n");
+    scanf("%d", &c);
+    printf("Ahora deberá ingresar valores para el Arreglo\n");
+    printf("Ingrese un valor para a[0]\n");
+    scanf("%d", &a[0]);
+    printf("Ingrese un valor para a[1]\n");
+    scanf("%d", &a[1]);
+    printf("Ingrese un valor para a[2]\n");
+    scanf("%d", &a[2]);
+    printf("Ingrese un valor para a[3]\n");
+    scanf("%d", &a[3]);
+
+    i = 0; 
+    c = 0; 
+    printf("\n");
+
+    while (i<4) {
+        printf("Estado numero %d: \n", i+1);
+        printf("a[%d] = %d\n", i, a[i]);
+        if (a[i]>0) {
+            c = c + 1 ;
+        }
+        i = i + 1 ;
+
+        printf("c = %d\n",c);
+        printf("i = %d\n",i);
+    }
+    printf("\nEstado final: \ni = %d \nc = %d\n", i, c);
+        
+    return 0;
+}
+
+
+
+//Ejercicio 10
 
 
 
