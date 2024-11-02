27ad65391ae08a5bba249113274dc6c245dfc42e
Agustin <82238717+AgU131@users.noreply.github.com>
2024-11-01 22:41:18 -0300

    Add files via upload

--- Practicos/soluciuonesPractico4enC/intercambio_arreglos.c
+++ Practicos/soluciuonesPractico4enC/intercambio_arreglos.c
@@ -5,6 +5,17 @@
 
 //Ejercicio 6
 
+int sumatoria(int a[], int tam){
+    int res = 0, i = 0;
+    while (i < res) {
+        
+    }
+    
+    
+    return res;
+}
+
+
 int main () {
 
     return 0;
