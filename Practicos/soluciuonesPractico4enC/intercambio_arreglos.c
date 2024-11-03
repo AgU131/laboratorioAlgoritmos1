ac2646ffaec27abd87a5bfdf1ad96b804f54627f
Agustin <82238717+AgU131@users.noreply.github.com>
2024-11-02 23:41:38 -0300

    Add files via upload

--- Practicos/soluciuonesPractico4enC/intercambio_arreglos.c
+++ Practicos/soluciuonesPractico4enC/intercambio_arreglos.c
@@ -1,7 +1,9 @@
+#include <stdio.h>  // solo este es necesario aca
 #include <stdlib.h>
-#include <stdio.h>
 #include <assert.h>
 #include <stdbool.h>
+
+
 
 //Ejercicio 6
 
@@ -12,13 +14,34 @@ int sumatoria(int a[], int tam){
         i = i + 1;
     }
     
-    
     return res;
 }
 
+void pedir_arreglos( int a[], int tam) {
+    int i = 0, e = 0;
+    while (i<tam) {
+        printf("Ingrese un valor para el lugar %d en el arreglo 'a'\n", i);
+        scanf("%d", &e);   // aca en vez de &e podria ir &a[i] pero se hace un poco engorroso de leer
+        a[i] = e ;  //mejor practica es esto, asignarle a un valor (como e) y ponerlo ahi para que se vea mas lindo
+        i = i + 1;
+    }
+}
 
 int main () {
+    int a[4], res = 0;
 
+    pedir_arreglos(a, 4) ;  //aca no es necesario asignarle ningun valor ya que de por si modifica el valor dentro del arreglo a
+    res = sumatoria(a, 4);
+
+    printf("El resultado es %i\n", res);
     return 0;
 }
+
+
+
+
+
+//Pedir los valores de las variables
+    // printf("Ingrese un valor para i\n");
+    // scanf("%d", &i);
 
